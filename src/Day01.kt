5c543f5ba36bcb3cc52e4725e10d07f88ca50463
Michael Czolko <michael@czolko.cz>
2021-12-02 11:20:41 +0100

    Day 1 and Day 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,39 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun part1(input: List<Int>): Int {
+        var prevNum = 0
+        var i = 0
+
+        input.forEach {
+            if (prevNum != 0) {
+                if (it > prevNum) i++
+            }
+            prevNum = it
+        }
+
+        return i
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<Int>): Int {
+        var prevNum = 0
+        var i = 0
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        input.forEachIndexed { index, it ->
+            if (index > input.size - 3) return i
+
+            val num = it + input[index + 1 ] + input[index + 2]
+
+            if (prevNum != 0) {
+                if (num > prevNum) i++
+            }
+            prevNum = num
+        }
+
+        return i
+    }
 
     val input = readInput("Day01")
+        .map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
---
+++ src/Day02.kt
@@ -0,0 +1,42 @@
+fun main() {
+    fun part1(input: List<Pair<String, Int>>): Int {
+        var x = 0
+        var y = 0
+
+        input.forEach {
+            when (it.first) {
+                "up"      -> y -= it.second
+                "down"    -> y += it.second
+                "forward" -> x += it.second
+            }
+        }
+
+        return x * y
+    }
+
+    fun part2(input: List<Pair<String, Int>>): Int {
+        var x = 0
+        var y = 0
+        var aim = 0
+
+        input.forEach {
+            when (it.first) {
+                "up"      -> aim -= it.second
+                "down"    -> aim += it.second
+                "forward" -> {
+                    x += it.second
+                    y += it.second * aim
+                }
+            }
+        }
+
+        return x * y
+    }
+
+    val input = readInput("Day02")
+        .map { it.split(' ') }
+        .map { Pair(it[0], it[1].toInt()) }
+
+    println(part1(input))
+    println(part2(input))
+}
